3a7a0b1a293ed704693487c6d143419779b8bed7
Stefan Ziegler <edi.gonzales@gmail.com>
2024-09-27 09:26:48 +0200

    wip

--- src/test/java/ch/so/agi/dmav/ConfigParserTest.java
+++ src/test/java/ch/so/agi/dmav/ConfigParserTest.java
@@ -10,10 +10,10 @@ public class ConfigParserTest {
 
     @Test
     void parseConfigFileOk() throws Exception {
-        Map<String,String> properties = ConfigParser.read(Paths.get("src/test/data/configparser/myconfig_local.ini"));
+        Map<String,String> properties = ConfigParser.read(Paths.get("src/test/data/merger/myconfig_local.ini"));
         
-        assertEquals("src/test/data/configparser/DMAV_FixpunkteAVKategorie2_V1_0.${fosnr}.xtf", properties.get("DMAV_FixpunkteAVKategorie2_V1_0"));
-        assertEquals("src/test/data/configparser/DMAV_PLZ_Ortschaft_V1_0.${fosnr}.xtf", properties.get("DMAV_PLZ_Ortschaft_V1_0"));
+        assertEquals("src/test/data/merger/DMAV_FixpunkteAVKategorie2_V1_0.${fosnr}.xtf", properties.get("DMAV_FixpunkteAVKategorie2_V1_0"));
+        assertEquals("src/test/data/merger/DMAV_PLZ_Ortschaft_V1_0.${fosnr}.xtf", properties.get("DMAV_PLZ_Ortschaft_V1_0"));
         
         assertEquals(2, properties.size());
     }
